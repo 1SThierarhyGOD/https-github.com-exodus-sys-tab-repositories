7ab80298d1f46c2f222f1caf5c718a586735d6b0
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:56:10 +0200

    Initialize jeliknes/identity-demo

---
+++ src/Servers/Connections.Abstractions/src/Features/ITlsHandshakeFeature.cs
@@ -0,0 +1,58 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Security.Authentication;
+
+#if NETCOREAPP
+using System.Net.Security;
+#endif
+
+namespace Microsoft.AspNetCore.Connections.Features;
+
+/// <summary>
+/// Represents the details about the TLS handshake.
+/// </summary>
+public interface ITlsHandshakeFeature
+{
+    /// <summary>
+    /// Gets the <see cref="SslProtocols"/>.
+    /// </summary>
+    SslProtocols Protocol { get; }
+
+#if NETCOREAPP
+    /// <summary>
+    /// Gets the <see cref="TlsCipherSuite"/>.
+    /// </summary>
+    TlsCipherSuite? NegotiatedCipherSuite => null;
+#endif
+
+    /// <summary>
+    /// Gets the <see cref="CipherAlgorithmType"/>.
+    /// </summary>
+    CipherAlgorithmType CipherAlgorithm { get; }
+
+    /// <summary>
+    /// Gets the cipher strength.
+    /// </summary>
+    int CipherStrength { get; }
+
+    /// <summary>
+    /// Gets the <see cref="HashAlgorithmType"/>.
+    /// </summary>
+    HashAlgorithmType HashAlgorithm { get; }
+
+    /// <summary>
+    /// Gets the hash strength.
+    /// </summary>
+    int HashStrength { get; }
+
+    /// <summary>
+    /// Gets the <see cref="KeyExchangeAlgorithm"/>.
+    /// </summary>
+    ExchangeAlgorithmType KeyExchangeAlgorithm { get; }
+
+    /// <summary>
+    /// Gets the key exchange algorithm strength.
+    /// </summary>
+    int KeyExchangeStrength { get; }
+}
