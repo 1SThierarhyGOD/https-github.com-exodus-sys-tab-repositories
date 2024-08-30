04f2264ecffb5515c4624bce60aa8479f54a993b
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:13:11 +0200

    Initialize javiercn/dev-certs-linux-trust

---
+++ src/Http/Http.Extensions/src/RequestDelegateFactoryOptions.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.AspNetCore.Http;
+
+/// <summary>
+/// Options for controlling the behavior of the <see cref="RequestDelegate" /> when created using <see cref="RequestDelegateFactory" />.
+/// </summary>
+public sealed class RequestDelegateFactoryOptions
+{
+    /// <summary>
+    /// The <see cref="IServiceProvider"/> instance used to access application services.
+    /// </summary>
+    public IServiceProvider? ServiceProvider { get; init; }
+
+    /// <summary>
+    /// The list of route parameter names that are specified for this handler.
+    /// </summary>
+    public IEnumerable<string>? RouteParameterNames { get; init; }
+
+    /// <summary>
+    /// Controls whether the <see cref="RequestDelegate"/> should throw a <see cref="BadHttpRequestException"/> in addition to
+    /// writing a <see cref="LogLevel.Debug"/> log when handling invalid requests.
+    /// </summary>
+    public bool ThrowOnBadRequest { get; init; }
+
+    /// <summary>
+    /// Prevent the <see cref="RequestDelegateFactory" /> from inferring a parameter should be bound from the request body without an attribute that implements <see cref="IFromBodyMetadata"/>.
+    /// </summary>
+    public bool DisableInferBodyFromParameters { get; init; }
+
+    /// <summary>
+    /// The list of filters that must run in the pipeline for a given route handler.
+    /// </summary>
+    public IReadOnlyList<Func<RouteHandlerContext, RouteHandlerFilterDelegate, RouteHandlerFilterDelegate>>? RouteHandlerFilterFactories { get; init; }
+
+    /// <summary>
+    /// The initial endpoint metadata to add as part of the creation of the <see cref="RequestDelegateResult.RequestDelegate"/>.
+    /// </summary>
+    /// <remarks>
+    /// This metadata will be included in <see cref="RequestDelegateResult.EndpointMetadata" /> <b>before</b> any metadata inferred during creation of the
+    /// <see cref="RequestDelegateResult.RequestDelegate"/> and <b>before</b> any metadata provided by types in the delegate signature that implement
+    /// <see cref="IEndpointMetadataProvider" /> or <see cref="IEndpointParameterMetadataProvider" />, i.e. this metadata will be less specific than any
+    /// inferred by the call to <see cref="RequestDelegateFactory.Create(Delegate, RequestDelegateFactoryOptions?)"/>.
+    /// </remarks>
+    public IEnumerable<object>? InitialEndpointMetadata { get; init; }
+}
