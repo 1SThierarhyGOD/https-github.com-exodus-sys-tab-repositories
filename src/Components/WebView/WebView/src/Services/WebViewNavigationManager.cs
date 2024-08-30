286e0955d7adeed7ddeba2bfea77b469dbad710d
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:21:02 +0200

    Initialize t-dagenkin/http3cat

---
+++ src/Components/WebView/WebView/src/Services/WebViewNavigationManager.cs
@@ -0,0 +1,27 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.Components.WebView.Services;
+
+internal sealed class WebViewNavigationManager : NavigationManager
+{
+    private IpcSender _ipcSender;
+
+    public void AttachToWebView(IpcSender ipcSender, string baseUrl, string initialUrl)
+    {
+        _ipcSender = ipcSender;
+        Initialize(baseUrl, initialUrl);
+    }
+
+    public void LocationUpdated(string newUrl, string? state, bool intercepted)
+    {
+        Uri = newUrl;
+        HistoryEntryState = state;
+        NotifyLocationChanged(intercepted);
+    }
+
+    protected override void NavigateToCore(string uri, NavigationOptions options)
+    {
+        _ipcSender.Navigate(uri, options);
+    }
+}
---
+++ src/Servers/Kestrel/Core/src/Middleware/LoggingDuplexPipe.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO.Pipelines;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal;
+
+internal sealed class LoggingDuplexPipe : DuplexPipeStreamAdapter<LoggingStream>
+{
+    public LoggingDuplexPipe(IDuplexPipe transport, ILogger logger) :
+        base(transport, stream => new LoggingStream(stream, logger))
+    {
+    }
+}
