99631e873b98fd5467ef772e5372eb7bcf1e7adf
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:22:51 +0200

    Initialize davidfowl/indicate-completion

---
+++ src/Servers/IIS/IIS/src/Core/NativeSafeHandle.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+
+namespace Microsoft.AspNetCore.Server.IIS.Core;
+
+internal sealed class NativeSafeHandle : SafeHandle
+{
+    public override bool IsInvalid => handle == IntPtr.Zero;
+
+    public NativeSafeHandle(IntPtr handle) : base(IntPtr.Zero, ownsHandle: true)
+    {
+        this.handle = handle;
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        handle = IntPtr.Zero;
+
+        return true;
+    }
+}
