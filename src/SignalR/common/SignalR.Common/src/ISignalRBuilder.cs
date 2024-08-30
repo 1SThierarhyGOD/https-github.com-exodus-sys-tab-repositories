06db96e35c8cd89ec10066d7bfe2750d26417f6e
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:08:26 +0200

    Initialize halter73/configureawait-false

---
+++ src/SignalR/common/SignalR.Common/src/ISignalRBuilder.cs
@@ -0,0 +1,17 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.SignalR;
+
+/// <summary>
+/// A builder abstraction for configuring SignalR object instances.
+/// </summary>
+public interface ISignalRBuilder
+{
+    /// <summary>
+    /// Gets the builder service collection.
+    /// </summary>
+    IServiceCollection Services { get; }
+}
