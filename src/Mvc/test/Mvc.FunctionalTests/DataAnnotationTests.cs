72a8b0c45c3cb04749cbd4e9c1b4a2c255769e64
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:05:33 +0200

    Initialize halter73/30025

---
+++ src/Mvc/test/Mvc.FunctionalTests/DataAnnotationTests.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using RazorWebSite;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.FunctionalTests;
+
+public class DataAnnotationTests : IClassFixture<MvcTestFixture<StartupDataAnnotations>>
+{
+    private HttpClient Client { get; set; }
+
+    public DataAnnotationTests(MvcTestFixture<StartupDataAnnotations> fixture)
+    {
+        var factory = fixture.Factories.FirstOrDefault() ?? fixture.WithWebHostBuilder(builder =>
+        {
+            builder.UseStartup<StartupDataAnnotations>();
+        });
+        Client = factory.CreateDefaultClient();
+    }
+
+    private const string EnumUrl = "http://localhost/Enum/Enum";
+
+    [Fact]
+    public async Task DataAnnotationLocalizationOfEnums_FromDataAnnotationLocalizerProvider()
+    {
+        // Arrange & Act
+        var response = await Client.GetAsync(EnumUrl);
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains("FirstOptionDisplay from singletype", content);
+    }
+}
