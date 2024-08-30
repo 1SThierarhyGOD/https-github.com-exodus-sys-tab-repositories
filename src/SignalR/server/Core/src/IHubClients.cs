c0862cabca08e4535f86227e4f4c7927c5df9728
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:11:22 +0200

    Initialize halter73/fix-http-sln-filter

---
+++ src/SignalR/server/Core/src/IHubClients.cs
@@ -0,0 +1,9 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.SignalR;
+
+/// <summary>
+/// An abstraction that provides access to client connections.
+/// </summary>
+public interface IHubClients : IHubClients<IClientProxy> { }
