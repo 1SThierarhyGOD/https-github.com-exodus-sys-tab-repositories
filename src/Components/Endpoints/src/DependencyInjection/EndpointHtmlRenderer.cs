a0e6abfe51026ffc977e638c5804c584dbb09a8e
S <sebastian.radzewicz@wp.pl>
2024-08-30 17:53:06 +0200

    Initialize surayya-MS/main

---
+++ src/Components/Endpoints/src/DependencyInjection/EndpointHtmlRenderer.cs
@@ -0,0 +1,112 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.HtmlRendering.Infrastructure;
+using Microsoft.AspNetCore.Components.Infrastructure;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web.HtmlRendering;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.AspNetCore.Components.Endpoints;
+
+/// <summary>
+/// A <see cref="StaticHtmlRenderer"/> subclass which is also the implementation of the
+/// <see cref="IComponentPrerenderer"/> DI service. This is the underlying mechanism shared by:
+///
+/// * Html.RenderComponentAsync (the earliest prerendering mechanism - a Razor HTML helper)
+/// * ComponentTagHelper (the primary prerendering mechanism before .NET 8)
+/// * RazorComponentResult and RazorComponentEndpoint (the primary prerendering mechanisms since .NET 8)
+///
+/// EndpointHtmlRenderer wraps the underlying <see cref="Web.HtmlRenderer"/> mechanism, annotating the
+/// output with prerendering markers so the content can later switch into interactive mode when used with
+/// blazor.*.js. It also deals with initializing the standard component DI services once per request.
+///
+/// Note that EndpointHtmlRenderer doesn't deal with streaming SSR since that's not applicable to Html.RenderComponentAsync
+/// or ComponentTagHelper, because they don't control the entire response. Streaming SSR is a layer around this implemented
+/// only inside RazorComponentResult/RazorComponentEndpoint.
+/// </summary>
+internal sealed partial class EndpointHtmlRenderer : StaticHtmlRenderer, IComponentPrerenderer
+{
+    private readonly IServiceProvider _services;
+    private Task? _servicesInitializedTask;
+    private Action<IEnumerable<HtmlComponentBase>>? _onContentUpdatedCallback;
+
+    public EndpointHtmlRenderer(IServiceProvider serviceProvider, ILoggerFactory loggerFactory)
+        : base(serviceProvider, loggerFactory)
+    {
+        _services = serviceProvider;
+    }
+
+    private static async Task InitializeStandardComponentServicesAsync(HttpContext httpContext)
+    {
+        var navigationManager = (IHostEnvironmentNavigationManager)httpContext.RequestServices.GetRequiredService<NavigationManager>();
+        navigationManager?.Initialize(GetContextBaseUri(httpContext.Request), GetFullUri(httpContext.Request));
+
+        var authenticationStateProvider = httpContext.RequestServices.GetService<AuthenticationStateProvider>() as IHostEnvironmentAuthenticationStateProvider;
+        if (authenticationStateProvider != null)
+        {
+            var authenticationState = new AuthenticationState(httpContext.User);
+            authenticationStateProvider.SetAuthenticationState(Task.FromResult(authenticationState));
+        }
+
+        // It's important that this is initialized since a component might try to restore state during prerendering
+        // (which will obviously not work, but should not fail)
+        var componentApplicationLifetime = httpContext.RequestServices.GetRequiredService<ComponentStatePersistenceManager>();
+        await componentApplicationLifetime.RestoreStateAsync(new PrerenderComponentApplicationStore());
+    }
+
+    public void OnContentUpdated(Action<IEnumerable<HtmlComponentBase>> callback)
+    {
+        if (_onContentUpdatedCallback is not null)
+        {
+            // The framework is the only user of this internal API, so it's OK to have an arbitrary limit like this
+            throw new InvalidOperationException($"{nameof(OnContentUpdated)} can only be called once.");
+        }
+
+        _onContentUpdatedCallback = callback;
+    }
+
+    protected override Task UpdateDisplayAsync(in RenderBatch renderBatch)
+    {
+        var count = renderBatch.UpdatedComponents.Count;
+        if (count > 0 && _onContentUpdatedCallback is not null)
+        {
+            // TODO: Deduplicate these. There's no reason ever to include the same component more than once in a single batch,
+            // as we're rendering its whole final state, not the intermediate diffs.
+            var htmlComponents = new List<HtmlComponentBase>(count);
+            for (var i = 0; i < count; i++)
+            {
+                ref var diff = ref renderBatch.UpdatedComponents.Array[i];
+                htmlComponents.Add(new HtmlComponentBase(this, diff.ComponentId));
+            }
+
+            _onContentUpdatedCallback(htmlComponents);
+        }
+
+        return base.UpdateDisplayAsync(renderBatch);
+    }
+
+    private static string GetFullUri(HttpRequest request)
+    {
+        return UriHelper.BuildAbsolute(
+            request.Scheme,
+            request.Host,
+            request.PathBase,
+            request.Path,
+            request.QueryString);
+    }
+
+    private static string GetContextBaseUri(HttpRequest request)
+    {
+        var result = UriHelper.BuildAbsolute(request.Scheme, request.Host, request.PathBase);
+
+        // PathBase may be "/" or "/some/thing", but to be a well-formed base URI
+        // it has to end with a trailing slash
+        return result.EndsWith('/') ? result : result += "/";
+    }
+}
---
+++ src/Http/Http.Extensions/gen/StaticRouteHandlerModel/Endpoint.cs
@@ -0,0 +1,164 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Analyzers.Infrastructure;
+using Microsoft.AspNetCore.App.Analyzers.Infrastructure;
+using Microsoft.AspNetCore.Http.RequestDelegateGenerator.StaticRouteHandlerModel.Emitters;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.AspNetCore.Http.RequestDelegateGenerator.StaticRouteHandlerModel;
+
+internal class Endpoint
+{
+    public Endpoint(IInvocationOperation operation, WellKnownTypes wellKnownTypes, SemanticModel semanticModel)
+    {
+        Operation = operation;
+        Location = GetLocation(operation);
+        HttpMethod = GetHttpMethod(operation);
+        EmitterContext = new EmitterContext();
+
+        if (!operation.TryGetRouteHandlerPattern(out var routeToken))
+        {
+            Diagnostics.Add(Diagnostic.Create(DiagnosticDescriptors.UnableToResolveRoutePattern, Operation.Syntax.GetLocation()));
+            return;
+        }
+
+        RoutePattern = routeToken.ValueText;
+
+        if (!operation.TryGetRouteHandlerMethod(semanticModel, out var method) || method == null)
+        {
+            Diagnostics.Add(Diagnostic.Create(DiagnosticDescriptors.UnableToResolveMethod, Operation.Syntax.GetLocation()));
+            return;
+        }
+
+        Response = new EndpointResponse(method, wellKnownTypes);
+        if (Response.IsAnonymousType)
+        {
+            Diagnostics.Add(Diagnostic.Create(DiagnosticDescriptors.UnableToResolveAnonymousReturnType, Operation.Syntax.GetLocation()));
+            return;
+        }
+
+        EmitterContext.HasJsonResponse = Response is not { ResponseType: { IsSealed: true } or { IsValueType: true } };
+        IsAwaitable = Response?.IsAwaitable == true;
+
+        if (method.Parameters.Length == 0)
+        {
+            return;
+        }
+
+        var parameters = new EndpointParameter[method.Parameters.Length];
+
+        for (var i = 0; i < method.Parameters.Length; i++)
+        {
+            var parameter = new EndpointParameter(this, method.Parameters[i], wellKnownTypes);
+
+            switch (parameter.Source)
+            {
+                case EndpointParameterSource.BindAsync:
+                    IsAwaitable = true;
+                    switch (parameter.BindMethod)
+                    {
+                        case BindabilityMethod.IBindableFromHttpContext:
+                        case BindabilityMethod.BindAsyncWithParameter:
+                            NeedsParameterArray = true;
+                            break;
+                    }
+                    break;
+                case EndpointParameterSource.JsonBody:
+                case EndpointParameterSource.JsonBodyOrService:
+                    IsAwaitable = true;
+                    break;
+                case EndpointParameterSource.Unknown:
+                    Diagnostics.Add(Diagnostic.Create(
+                        DiagnosticDescriptors.UnableToResolveParameterDescriptor,
+                        Operation.Syntax.GetLocation(),
+                        parameter.SymbolName));
+                    break;
+            }
+
+            parameters[i] = parameter;
+        }
+
+        Parameters = parameters;
+
+        EmitterContext.HasJsonBodyOrService = Parameters.Any(parameter => parameter.Source == EndpointParameterSource.JsonBodyOrService);
+        EmitterContext.HasJsonBody = Parameters.Any(parameter => parameter.Source == EndpointParameterSource.JsonBody);
+        EmitterContext.HasRouteOrQuery = Parameters.Any(parameter => parameter.Source == EndpointParameterSource.RouteOrQuery);
+        EmitterContext.HasBindAsync = Parameters.Any(parameter => parameter.Source == EndpointParameterSource.BindAsync);
+        EmitterContext.HasParsable = Parameters.Any(parameter => parameter.IsParsable);
+    }
+
+    public string HttpMethod { get; }
+    public bool IsAwaitable { get; }
+    public bool NeedsParameterArray { get; }
+    public string? RoutePattern { get; }
+    public EmitterContext EmitterContext { get;  }
+    public EndpointResponse? Response { get; }
+    public EndpointParameter[] Parameters { get; } = Array.Empty<EndpointParameter>();
+    public List<Diagnostic> Diagnostics { get; } = new List<Diagnostic>();
+
+    public (string File, int LineNumber) Location { get; }
+    public IInvocationOperation Operation { get; }
+
+    public override bool Equals(object o) =>
+        o is Endpoint other && Location == other.Location && SignatureEquals(this, other);
+
+    public override int GetHashCode() =>
+        HashCode.Combine(Location, GetSignatureHashCode(this));
+
+    public static bool SignatureEquals(Endpoint a, Endpoint b)
+    {
+        if (!string.Equals(a.Response?.WrappedResponseType, b.Response?.WrappedResponseType, StringComparison.Ordinal) ||
+            !a.HttpMethod.Equals(b.HttpMethod, StringComparison.Ordinal) ||
+            a.Parameters.Length != b.Parameters.Length)
+        {
+            return false;
+        }
+
+        for (var i = 0; i < a.Parameters.Length; i++)
+        {
+            if (!a.Parameters[i].SignatureEquals(b.Parameters[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int GetSignatureHashCode(Endpoint endpoint)
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(endpoint.Response?.WrappedResponseType);
+        hashCode.Add(endpoint.HttpMethod);
+
+        foreach (var parameter in endpoint.Parameters)
+        {
+            hashCode.Add(parameter.Type, SymbolEqualityComparer.Default);
+        }
+
+        return hashCode.ToHashCode();
+    }
+
+    private static (string, int) GetLocation(IInvocationOperation operation)
+    {
+        var filePath = operation.Syntax.SyntaxTree.FilePath;
+        var span = operation.Syntax.SyntaxTree.GetLineSpan(operation.Syntax.Span);
+        var lineNumber = span.StartLinePosition.Line + 1;
+        return (filePath, lineNumber);
+    }
+
+    private static string GetHttpMethod(IInvocationOperation operation)
+    {
+        var syntax = (InvocationExpressionSyntax)operation.Syntax;
+        var expression = (MemberAccessExpressionSyntax)syntax.Expression;
+        var name = (IdentifierNameSyntax)expression.Name;
+        var identifier = name.Identifier;
+        return identifier.ValueText;
+    }
+}
